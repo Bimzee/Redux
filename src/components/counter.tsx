d7e79d2b2f379a7266575f8bb556a550adfbce26
Bimal Varghese <bimal.varghese@boeing.com>
2023-02-08 19:14:04 +0530

    Implemented Connect method in Functional Component

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,27 +1,64 @@
-import { useDispatch, useSelector } from "react-redux";
+import {
+  ReactElement,
+  JSXElementConstructor,
+  ReactFragment,
+  ReactPortal,
+} from "react";
+import { useDispatch, useSelector, connect } from "react-redux";
 import classes from "./Counter.module.css";
 
-const Counter = () => {
-  const count = useSelector((state: any) => state.counter);
-  const dispatch = useDispatch();
+const Counter = (props: {
+  inc: () => void;
+  dec: () => void;
+  count:
+    | string
+    | number
+    | boolean
+    | ReactElement<any, string | JSXElementConstructor<any>>
+    | ReactFragment
+    | ReactPortal
+    | null
+    | undefined;
+}) => {
+  //   const count = useSelector((state: any) => state.counter);
+  //   const dispatch = useDispatch();
   const incHandler = () => {
-    dispatch({ type: "INC" });
+    // dispatch({ type: "INC" });
+    props.inc();
   };
   const decHandler = () => {
-    dispatch({ type: "DEC" });
+    // dispatch({ type: "DEC" });
+    props.dec();
   };
   const toggleCounterHandler = () => {};
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{count}</div>
+      <div className={classes.value}>{props.count}</div>
       <div>
         <button onClick={incHandler}>INC</button>
-        <button onClick={decHandler} >DEC</button>
+        <button onClick={decHandler}>DEC</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
 };
 
-export default Counter;
+const mapStateToProps = (state: { counter: any }) => {
+  return {
+    count: state.counter,
+  };
+};
+
+const mapDispatchToProps = (dispatch: (arg0: { type: string }) => any) => {
+  return {
+    inc: () => dispatch({ type: "INC" }),
+    dec: () => dispatch({ type: "DEC" }),
+  };
+};
+// connect accepts 2 functions as parameters,
+// mapStateToProps equivalent to useSelectot. this enables us to access the store
+// mapDispatchToProps equivalent to useDispatch. used to invode dispatch methods
+// The argumens provided to connect will be available in props of the component.
+// After the arguments the connect invokes the Component itself.
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
